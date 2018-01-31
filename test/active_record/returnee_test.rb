d1bdd654f8c32c33421c86e93e2da4f51c36eda5
yalab <rudeboyjet@gmail.com>
2018-01-31 17:25:12 +0900

    T

--- test/active_record/returnee_test.rb
+++ test/active_record/returnee_test.rb
@@ -33,6 +33,10 @@ class ActiveRecord::ReturneeTest < Minitest::Test
     assert_equal fixture_path(:user_oauths).read, ActiveRecord::Returnee.new("user_oauths").to_create_table
   end
 
+  def test_create_user_oauths
+    assert_equal fixture_path(:pictures).read, ActiveRecord::Returnee.new("pictures").to_create_table
+  end
+
   def test_members_dependencies
     assert_equal [:users, :groups], ActiveRecord::Returnee.new("members").dependencies
   end
---
+++ test/fixtures/migrations/20180131080520_create_pictures.rb
@@ -0,0 +1,8 @@
+class CreatePictures < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pictures do |t|
+      t.string :name
+      t.references :imageable, polymorphic: true, index: true
+    end
+  end
+end
